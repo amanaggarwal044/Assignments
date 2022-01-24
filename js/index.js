3030ce95554ae6930af9aa0162059c921de4c54c
amanaggarwal044 <66272591+amanaggarwal044@users.noreply.github.com>
2022-01-24 15:38:24 +0530

    list 3

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-function f1(){
 var list_items = document.querySelectorAll(".selector1>ul>li");
-console.log(list_items);
+// console.log(list_items);
+// console.log(a)
 list_items.forEach((list)=>{
 list.addEventListener('click',()=>{
     list_items.forEach((item)=>{    
-        console.log(item.children[0].classList.contains("active_list_item"));
-        if((item.classList.contains("active_list") && item.children[0].classList.contains("active_list_item")))
+        if(item.classList.contains("active_list") && item.children[0].classList.contains("active_list_item"))
         { 
-            console.log("in if loop");
+            // console.log("in if loop");
         }
         else{
             item.classList.toggle("active_list");
@@ -21,8 +20,8 @@ list.addEventListener('click',()=>{
 
 
         var prev_selected_item = document.querySelector(".selector1>ul>li>span.active_list_item");
-        console.log(prev_selected_item);
         
+        document.querySelector(`.${prev_selected_item.textContent.replaceAll(" ","_")}`).classList.remove("active_render_list_content");
         prev_selected_item.nextElementSibling.remove();
         prev_selected_item.classList.remove("active_list_item");
         prev_selected_item.parentElement.classList.remove("active_list");
@@ -30,15 +29,18 @@ list.addEventListener('click',()=>{
         list.classList.add("active_list");
         list.children[0].classList.add("active_list_item");
         list.insertAdjacentHTML("beforeend",`<i class="fa fa-angle-down list_icon" aria-hidden="true"></i>`);
-    
-        console.log(list);
-        console.log(list.parentElement);
-        list.parentElement.insertAdjacentHTML(
-            "afterbegin",
-            `${list.outerHTML}`
-          );
-          list.remove();
-f1();
+        var renderingClassName=list.children[0].textContent.replaceAll(" ", "_");
+        document.querySelector(`.${renderingClassName}`).classList.add("active_render_list_content");
+
+        // list.parentElement.insertAdjacentHTML(
+        // "afterbegin",
+        //     `${list.outerHTML}`
+        //   );
+        //   list.remove();
+
+
+
+
     
     }
 
@@ -48,8 +50,6 @@ f1();
 
     })
 
-}
 
-f1();
 
 
